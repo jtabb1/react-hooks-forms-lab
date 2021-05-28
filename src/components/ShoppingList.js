c25173ee079e7e98feb4784471bde6e5eb9a97b0
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-05-28 14:19:49 -0400

    note where search algorithm is

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,3 +1,7 @@
+// See the search algorithm command given by the official solution as
+// reproduced in the code below:
+//
+
 import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
@@ -14,7 +18,10 @@ function ShoppingList({ items, setItems, onItemFormSubmit }) {
   function onSearchChange(event) {
     setNameDemand(event.target.value);
   }
-
+  
+  // The search algorithm command is the last predicate element
+  // of the return statement of the variable below:
+  //
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All"
         && search === "") return true;
